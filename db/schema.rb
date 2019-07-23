639a13d90a66e630c333cf91571a806b04fb4745
Taiga Miwa <taiga.miwa888@gmail.com>
2019-07-23 18:01:06 +0900

    指摘点の修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_07_23_023741) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tasks", force: :cascade do |t|
     t.string "title", null: false
     t.string "body", null: false
