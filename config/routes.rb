9e82ce1bc02bac54ec7c653b0050c7bc97933da4
Taiga Miwa <taiga.miwa888@gmail.com>
2019-07-23 14:58:29 +0900

    ルーティングを定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root  'tasks#index'
+
+  get '/tasks', to: 'tasks#index'
+  get '/tasks/:id', to: 'tasks#show'
+
 end
