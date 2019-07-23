57dd4bc38f3ed0f58fbcd717e8c4759a503c354e
Taiga Miwa <taiga.miwa888@gmail.com>
2019-07-23 14:58:00 +0900

    Tasksコントローラーを作成、7アクションを記入

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,30 @@
+class TasksController < ApplicationController
+
+    def index
+        @tasks = Task.all
+    end
+
+    def new
+
+    end
+
+    def show
+        @task = Task.find(params[:id])
+    end
+
+    def create
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+end
