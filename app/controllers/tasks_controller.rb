9df804ef7714d7607540396acbed8657834867ab
Taiga Miwa <taiga.miwa888@gmail.com>
2019-07-24 11:36:11 +0900

    edit,updateアクション、ルーティングを定義

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -19,11 +19,12 @@ class TasksController < ApplicationController
     end
 
     def edit
-
+        @task = Task.find(params[:id])
     end
 
     def update
-
+        @task = Task.find(params[:id])
+        @task.update(task_params)
     end
 
     def destroy
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get '/tasks/new', to: 'tasks#new'
   post '/tasks', to: 'tasks#create'
   get '/tasks/:id', to: 'tasks#show'
+  get '/tasks/:id/edit', to: 'tasks#edit'
+  patch '/tasks/:id', to: 'tasks#update'
 
 end
