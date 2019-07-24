938e25ab07866bdcbe333a99ace90861ba0cad03
Taiga Miwa <taiga.miwa888@gmail.com>
2019-07-24 18:32:17 +0900

    Taskモデルにバリデーションを設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,8 @@
 class Task < ApplicationRecord
-    enum status: { waiting: 0, working: 1, finished: 2}
+	enum status: { waiting: 0, working: 1, finished: 2}
+	with_options presence: true do
+		validates :title
+		validates :body
+		validates :status
+	end
 end
