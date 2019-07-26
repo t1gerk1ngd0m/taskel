293fb6a85a2e93c0fa3f9ea53031b86d935c2905
Taiga Miwa <taiga.miwa888@gmail.com>
2019-07-26 19:10:49 +0900

    タスク新規投稿テストの完成

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -18,16 +18,15 @@ RSpec.describe 'Tasks', type: :system do
       fill_in 'task_title', with: task.title
       fill_in 'task_body', with: task.body
       select task.status ,from: 'task_status'
-
       click_button '作成する'
-      binding.pry
-      # expect(response).to redirect_to(root_path)
+      expect(page).to have_content("タスク一覧")
       expect(page).to have_content("タスクを作成しました")
     end
 
     scenario 'fail in task creation', type: :system do
       visit new_task_path
       click_button '作成する'
+      expect(page).to_not have_content("タスク一覧")
       expect(page).to have_content("タスクの作成に失敗しました")
     end
   end
