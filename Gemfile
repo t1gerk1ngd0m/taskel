51b54c1e0f32084d5fd802deca53248def74de18
Taiga Miwa <taiga.miwa888@gmail.com>
2019-07-26 12:18:56 +0900

    factory_botの導入

--- Gemfile
+++ Gemfile
@@ -43,6 +43,7 @@ group :development, :test do
   gem 'pry-byebug'
   gem 'pry-doc'
   gem 'rspec-rails'
+  gem 'factory_bot_rails'
 end
 
 group :development do
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 describe Task do
   describe '#create' do
     it "is invalid without a title" do
-      task = Task.new(title: "", body: "本文", deadline: '2018-06-07', status: 2, file: "0000000" )
+      task = build(:task, title: "")
       task.valid?
       expect(task.errors[:title]).to include("を入力してください")
     end
     it "is invalid without a body" do
-      task = Task.new(title: "タイトル", body: "", deadline: '2018-06-07', status: 2, file: "0000000" )
+      task = build(:task, body: "")
       task.valid?
       expect(task.errors[:body]).to include("を入力してください")
     end
     it "is invalid without a status" do
-      task = Task.new(title: "タイトル", body: "本文", deadline: '2018-06-07', status: "", file: "0000000" )
+      task = build(:task, status: "")
       task.valid?
       expect(task.errors[:status]).to include("を入力してください")
     end
