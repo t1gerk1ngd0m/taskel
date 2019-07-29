7bbd616543a6a494773c3b71daed7faebb42e5b6
Taiga Miwa <taiga.miwa888@gmail.com>
2019-07-29 10:11:07 +0900

    タスク編集テスト文を作成

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -15,19 +15,52 @@ RSpec.describe 'Tasks', type: :system do
 
     scenario 'succeed in task creation', type: :system do
       visit new_task_path
+
       fill_in 'task_title', with: task.title
       fill_in 'task_body', with: task.body
       select task.status ,from: 'task_status'
       click_button '作成する'
+
       expect(page).to have_content("タスク一覧")
       expect(page).to have_content("タスクを作成しました")
     end
 
     scenario 'fail in task creation', type: :system do
       visit new_task_path
+
       click_button '作成する'
+
       expect(page).to_not have_content("タスク一覧")
       expect(page).to have_content("タスクの作成に失敗しました")
+    end
+  end
+
+  feature 'edit page' do
+
+    before do
+      @task = Task.create(title: "タスクテスト", body: "タスクテスト本文", status: 1)
+    end
+
+    scenario 'succeed in task editation', type: :system do
+      visit edit_task_path(id: @task.id)
+
+      fill_in 'task_title', with: @task.title
+      fill_in 'task_body', with: @task.body
+      select @task.status ,from: 'task_status'
+      click_button '編集する'
+
+      expect(page).to have_content("タスク詳細")
+      expect(page).to have_content("タスクを編集しました")
+    end
+
+    scenario 'fail in task editation', type: :system do
+      visit edit_task_path(id: @task.id)
+
+      fill_in 'task_title', with: ''
+      click_button '編集する'
+
+      expect(page).to_not have_content("タスク詳細")
+      expect(page).to have_content("タスクの編集に失敗しました")
     end
   end
 
