dfa4dd0f68d84a2a0f1a1618a803bdf4d12461df
Taiga Miwa <taiga.miwa888@gmail.com>
2019-08-05 17:58:57 +0900

    テスト用インスタンスのstatus,日付を変更

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe 'Tasks', type: :system do
     given(:task) { Task.create(
       title: "タスクタイトル",
       body: "タスク本文",
-      status: 0
+      status: Task.statuses["waiting"]
     ) }
 
     scenario 'succeed in task creation', type: :system do
@@ -42,7 +42,7 @@ RSpec.describe 'Tasks', type: :system do
   feature 'edit page' do
 
     before do
-      @task = Task.create(title: "タスクテスト", body: "タスクテスト本文", status: 1)
+      @task = Task.create(title: "タスクテスト", body: "タスクテスト本文", status: Task.statuses["working"])
     end
 
     scenario 'succeed in task editation', type: :system do
@@ -74,10 +74,10 @@ RSpec.describe 'Tasks', type: :system do
   feature 'index page' do
 
     before do
-      @task = Task.create(title: "タスクテスト１", body: "タスクテスト本文１", status: 1, deadline: "2019-08-10")
-      @task = Task.create(title: "タスクテスト２", body: "タスクテスト本文２", status: 0, deadline: "2019-08-20", created_at: Time.current + 1.days)
-      @task = Task.create(title: "タスクテスト３", body: "タスクテスト本文３", status: 2, created_at: Time.current + 2.days)
-      @task = Task.create(title: "タスクテスト４", body: "タスクテスト本文４", status: 0, deadline: "2019-08-20", created_at: Time.current + 3.days)
+      @task = Task.create(title: "タスクテスト１", body: "タスクテスト本文１", status: Task.statuses["working"], deadline: Date.today + 10.days)
+      @task = Task.create(title: "タスクテスト２", body: "タスクテスト本文２", status: Task.statuses["waiting"], deadline: Date.today + 20.days, created_at: Time.current + 1.days)
+      @task = Task.create(title: "タスクテスト３", body: "タスクテスト本文３", status: Task.statuses["finished"], created_at: Time.current + 2.days)
+      @task = Task.create(title: "タスクテスト４", body: "タスクテスト本文４", status: Task.statuses["waiting"], deadline: Date.today + 20.days, created_at: Time.current + 3.days)
     end
 
     scenario 'sorted by creation date in default', type: :system do
