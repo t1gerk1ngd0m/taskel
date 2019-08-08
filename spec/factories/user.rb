a531ebbe75d13789dffea3c7068605b7675bdc8e
Taiga Miwa <taiga.miwa888@gmail.com>
2019-08-08 14:16:37 +0900

    テストの軽微な修正

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,6 +2,6 @@ FactoryBot.define do
   factory :user do
     sequence(:name) { |n| "test_user#{n}" }
     sequence(:email) { |n| "test_email#{n}@example.com" }
-    sequence(:password_digest) { |n| "password#{n}" }
+    sequence(:password) { |n| "password#{n}" }
   end
 end
