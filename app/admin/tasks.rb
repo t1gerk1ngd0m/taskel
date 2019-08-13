ccdc1cb4610de949d132030654d4fd19ac0f9c74
Taiga Miwa <taiga.miwa888@gmail.com>
2019-08-13 22:27:45 +0900

    ユーザー、タスク管理ページ作成

---
+++ app/admin/tasks.rb
@@ -0,0 +1,23 @@
+ActiveAdmin.register Task do
+  actions :index
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+  controller do
+    def scoped_collection
+      super
+      Task.includes(:user)
+    end
+  end
+
+end
---
+++ app/admin/users.rb
@@ -0,0 +1,50 @@
+ActiveAdmin.register User do
+  actions :all
+  permit_params :name, :email, :password
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+# permit_params :list, :of, :attributes, :on, :model
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+  # 一覧ページ検索条件
+  filter :name
+  filter :email
+
+  # 一覧ページ
+  index do
+    column :id
+    column :name
+    column :email
+    column "タスク数", :tasks do |user|
+      user.tasks.count
+    end
+    actions 
+  end
+
+  # 詳細ページ
+  show do
+    attributes_table do
+      row :id
+      row :name
+      row :email
+    end
+  end
+
+  # 新規作成/編集ページ
+  form do |f|
+    inputs  do
+      input :name
+      input :email
+      input :password
+    end
+    actions
+  end
+end
