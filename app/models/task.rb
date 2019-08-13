1dda660965b43b7169f8693fbb82a461064f5e3b
Taiga Miwa <taiga.miwa888@gmail.com>
2019-08-13 12:59:18 +0900

    検索機能をメソッドチェーンを用いて定義

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,22 +1,29 @@
 class Task < ApplicationRecord
-	# ログイン/ログアウト機能を通すため一旦optional: trueを追加
-	belongs_to :user, optional: true
-	enum status: { waiting: 0, working: 1, finished: 2}
-	enum priority: { low: 0, middle: 1, high: 2}
-	validates :title, presence: true
-	validates :body, presence: true
-	validates :status, presence: true
-	validates :priority, presence: true
+  # ログイン/ログアウト機能を通すため一旦optional: trueを追加
+  belongs_to :user, optional: true
+  enum status: { waiting: 0, working: 1, finished: 2}
+  enum priority: { low: 0, middle: 1, high: 2}
+  validates :title, presence: true
+  validates :body, presence: true
+  validates :status, presence: true
+  validates :priority, presence: true
 
-	def self.search(params)
-		# title,status共にblankでないとき（indexページ新規取得時）
-		if params[:title].blank? && params[:status].blank?
-			all
-		# statusで検索しないとき
-		elsif params[:status].empty?
-			where('title Like(?)', "%#{params[:title]}%")
-		elsif params[:title] && params[:status]
-			where('(title Like(?)) AND (status = ?)', "%#{params[:title]}%", params[:status])
-		end
-	end
-end
+  def self.search(params)
+    if params[:title].blank? && params[:status].blank?
+      all
+    elsif params[:status].empty?
+      search_title(params[:title])
+    elsif params[:title] && params[:status]
+      search_title(params[:title]).search_status(params[:status])
+    end
+  end
+
+  private
+  def self.search_title(title)
+    where('title Like(?)', "%#{title}%")
+  end
+
+  def self.search_status(status)
+    where('status = ?', status)
+  end
+  end
