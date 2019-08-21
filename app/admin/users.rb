340bdfa1a2b412aca430cf3c967f6d52732632e6
Taiga Miwa <taiga.miwa888@gmail.com>
2019-08-21 16:58:38 +0900

    Revert "Revert "管理ユーザーが0人になる状況を回避""
    This reverts commit c72e007d0d042ccbd4df7e480e48c10b9b9133b1.

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register User do
   actions :all
-  permit_params :name, :email, :password
+  permit_params :name, :email, :password, :role
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
