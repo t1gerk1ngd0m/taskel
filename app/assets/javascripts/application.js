f8ab7739bad32a5bee1cb7f66f84cf3482ba3611
Taiga Miwa <taiga.miwa888@gmail.com>
2019-08-21 13:52:06 +0900

    Revert "activeadminのviewの切り替え"
    This reverts commit d3b326add9132c89c6f195cbbf58609f44fe8afc.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,7 +14,7 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery3
-//= require_tree ./main
+//= require_tree .
 //= require popper
 //= require bootstrap-sprockets
 //= require moment
