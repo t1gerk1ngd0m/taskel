2a5f5a256f35aaf07ba0fe57959e0ba921643647
Taiga Miwa <taiga.miwa888@gmail.com>
2019-08-21 15:30:13 +0900

    Revert "指摘点を修正"
    This reverts commit 8797e27c133581547d4891da644cc34db1f64bf5.

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,11 +1,2 @@
 module TasksHelper
-  def alert_visible(task)
-    if task.deadline > Time.zone.today
-      link_to "#{task.title}の期限は明日です。期日:#{task.deadline}", task_path(task.id)
-    elsif task.deadline == Time.zone.today
-      link_to "#{task.title}の期限は本日です。期日:#{task.deadline}", task_path(task.id)
-    else task.deadline < Time.zone.today
-      link_to "#{task.title}の期限は超過しています。期日:#{task.deadline}", task_path(task.id)
-    end
-  end
 end
