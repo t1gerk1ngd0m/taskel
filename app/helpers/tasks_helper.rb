854e95e673327bea124893ab6998705219f3f691
Taiga Miwa <taiga.miwa888@gmail.com>
2019-08-23 17:28:07 +0900

    軽微な修正

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,11 +1,11 @@
 module TasksHelper
   def alert_visible(group, task)
     if task.deadline > Time.zone.today
-      link_to "#{task.title}の期限は明日です。期日:#{task.deadline}", task_path(task)
+      link_to "#{task.title}の期限は明日です。期日:#{task.deadline}", group_task_path(group, task)
     elsif task.deadline == Time.zone.today
-      link_to "#{task.title}の期限は本日です。期日:#{task.deadline}", task_path(task)
+      link_to "#{task.title}の期限は本日です。期日:#{task.deadline}", group_task_path(group, task)
     else task.deadline < Time.zone.today
-      link_to "#{task.title}の期限は超過しています。期日:#{task.deadline}", task_path(task)
+      link_to "#{task.title}の期限は超過しています。期日:#{task.deadline}", group_task_path(group, task)
     end
   end
 end
