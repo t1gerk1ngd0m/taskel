e917c2f139133567939e6ff34f6326d6caa0e17e
Taiga Miwa <taiga.miwa888@gmail.com>
2019-08-23 17:20:26 +0900

    既存のテストを修正

--- spec/factories/group_user.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :group_user do
-    association :user
-    association :group
-  end
-end
--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 describe Task, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
 
+  before do
+    @user = create(:user)
+    @group = create(:group, users: [@user])
+    task = create(:task, user_id: @user.id, group_id: @user.groups.ids[0])
+  end
+
   describe '#create' do
     it "is invalid without a title" do
       task = build(:task, title: "")
@@ -32,22 +38,18 @@ describe Task, type: :model do
   end
 
   describe 'search tasks' do
-    # before doでセットした値がなぜか各contextでセットされていないので一旦コメントアウト
-    # before do
-    #   task1 = create(:task, title: "taska_1", status: 'waiting')
-    #   task2 = create(:task, title: "task_2", status: 'working')
-    #   task3 = create(:task, title: "task_3", status: 'finished')
-    #   task4 = create(:task, title: "taskb_4", status: 'finished')
-    #   task5 = create(:task, title: "task_5", status: 'working')
-    # end
+    before do
+      @user = create(:user)
+      @group = create(:group, users: [@user])
+    end
 
     context "when a match is found searched with title" do
       it "returns tasks that match the search term" do
-        task1 = create(:task, title: "taska_1", status: 'waiting')
-        task2 = create(:task, title: "task_2", status: 'working')
-        task3 = create(:task, title: "task_3", status: 'finished')
-        task4 = create(:task, title: "taskb_4", status: 'finished')
-        task5 = create(:task, title: "task_5", status: 'working')
+        task1 = create(:task, title: "taska_1", status: 'waiting', user_id: @user.id, group_id: @user.groups.ids[0])
+        task2 = create(:task, title: "task_2", status: 'working', user_id: @user.id, group_id: @user.groups.ids[0])
+        task3 = create(:task, title: "task_3", status: 'finished', user_id: @user.id, group_id: @user.groups.ids[0])
+        task4 = create(:task, title: "taskb_4", status: 'finished', user_id: @user.id, group_id: @user.groups.ids[0])
+        task5 = create(:task, title: "task_5", status: 'working', user_id: @user.id, group_id: @user.groups.ids[0])
         params = { title: "task_", status: '' }
         expect(Task.search(params)).to include(task2, task3, task5)
       end
@@ -55,11 +57,11 @@ describe Task, type: :model do
 
     context "when no match is found searched with title" do
       it "returns an empty collection" do
-        task1 = create(:task, title: "taska_1", status: 'waiting')
-        task2 = create(:task, title: "task_2", status: 'working')
-        task3 = create(:task, title: "task_3", status: 'finished')
-        task4 = create(:task, title: "taskb_4", status: 'finished')
-        task5 = create(:task, title: "task_5", status: 'working')
+        task1 = create(:task, title: "taska_1", status: 'waiting', user_id: @user.id, group_id: @user.groups.ids[0])
+        task2 = create(:task, title: "task_2", status: 'working', user_id: @user.id, group_id: @user.groups.ids[0])
+        task3 = create(:task, title: "task_3", status: 'finished', user_id: @user.id, group_id: @user.groups.ids[0])
+        task4 = create(:task, title: "taskb_4", status: 'finished', user_id: @user.id, group_id: @user.groups.ids[0])
+        task5 = create(:task, title: "task_5", status: 'working', user_id: @user.id, group_id: @user.groups.ids[0])
         params = { title: "taskc", status: '' }
         expect(Task.search(params)).to be_empty
       end
@@ -67,11 +69,11 @@ describe Task, type: :model do
 
     context "when a match is found searched with status" do
       it "returns tasks that match the search term" do
-        task1 = create(:task, title: "taska_1", status: 'waiting')
-        task2 = create(:task, title: "task_2", status: 'working')
-        task3 = create(:task, title: "task_3", status: 'finished')
-        task4 = create(:task, title: "taskb_4", status: 'finished')
-        task5 = create(:task, title: "task_5", status: 'working')
+        task1 = create(:task, title: "taska_1", status: 'waiting', user_id: @user.id, group_id: @user.groups.ids[0])
+        task2 = create(:task, title: "task_2", status: 'working', user_id: @user.id, group_id: @user.groups.ids[0])
+        task3 = create(:task, title: "task_3", status: 'finished', user_id: @user.id, group_id: @user.groups.ids[0])
+        task4 = create(:task, title: "taskb_4", status: 'finished', user_id: @user.id, group_id: @user.groups.ids[0])
+        task5 = create(:task, title: "task_5", status: 'working', user_id: @user.id, group_id: @user.groups.ids[0])
         params = { title: "sk_", status: "#{Task.statuses[:working]}" }
         expect(Task.search(params)).to include(task2, task5)
       end
@@ -79,11 +81,11 @@ describe Task, type: :model do
 
     context "when no match is found searched with status" do
       it "returns an empty collection" do
-        task1 = create(:task, title: "taska_1", status: 'waiting')
-        task2 = create(:task, title: "task_2", status: 'working')
-        task3 = create(:task, title: "task_3", status: 'finished')
-        task4 = create(:task, title: "taskb_4", status: 'finished')
-        task5 = create(:task, title: "task_5", status: 'working')
+        task1 = create(:task, title: "taska_1", status: 'waiting', user_id: @user.id, group_id: @user.groups.ids[0])
+        task2 = create(:task, title: "task_2", status: 'working', user_id: @user.id, group_id: @user.groups.ids[0])
+        task3 = create(:task, title: "task_3", status: 'finished', user_id: @user.id, group_id: @user.groups.ids[0])
+        task4 = create(:task, title: "taskb_4", status: 'finished', user_id: @user.id, group_id: @user.groups.ids[0])
+        task5 = create(:task, title: "task_5", status: 'working', user_id: @user.id, group_id: @user.groups.ids[0])
         params = { title: "skc_", status: "#{Task.statuses[:finished]}" }
         expect(Task.search(params)).to be_empty
       end
@@ -91,11 +93,11 @@ describe Task, type: :model do
 
     context "when no match is found searched with status" do
       it "returns all collection" do
-        task1 = create(:task, title: "taska_1", status: 'waiting')
-        task2 = create(:task, title: "task_2", status: 'working')
-        task3 = create(:task, title: "task_3", status: 'finished')
-        task4 = create(:task, title: "taskb_4", status: 'finished')
-        task5 = create(:task, title: "task_5", status: 'working')
+        task1 = create(:task, title: "taska_1", status: 'waiting', user_id: @user.id, group_id: @user.groups.ids[0])
+        task2 = create(:task, title: "task_2", status: 'working', user_id: @user.id, group_id: @user.groups.ids[0])
+        task3 = create(:task, title: "task_3", status: 'finished', user_id: @user.id, group_id: @user.groups.ids[0])
+        task4 = create(:task, title: "taskb_4", status: 'finished', user_id: @user.id, group_id: @user.groups.ids[0])
+        task5 = create(:task, title: "task_5", status: 'working', user_id: @user.id, group_id: @user.groups.ids[0])
         params = { title: "", status: '' }
         expect(Task.search(params)).to include(task1, task2, task3, task4, task5)
       end
