339f48abe4ad17b0978b726a2d58c5a80463f2f1
Taiga Miwa <taiga.miwa888@gmail.com>
2019-08-30 11:42:13 +0900

    アクセスキーを修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+      aws_access_key_id: ENV['AWS_S3_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_S3_SECRET_ACCESS_KEY'],
       region: ENV['AWS_REGION']
     }
     config.fog_directory  = 'taskel-file'
